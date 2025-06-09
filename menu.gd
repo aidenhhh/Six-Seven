c8e94acc429857ba16b92b88c7854d86f3a741ac
abratu2226 <allstar555700@gmail.com>
2025-06-09 15:12:28 -0400

    added six levels and changed menu to go to level 1 when click play

--- menu.gd
+++ menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://level_selector.tscn")
+	get_tree().change_scene_to_file("res://Scenes/level1.tscn")
 
 
 func _on_rules_button_pressed() -> void:
