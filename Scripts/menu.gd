2e602e38c68142de8ad66f61ac22e99a4ae60af9
abratu2226 <allstar555700@gmail.com>
2025-06-10 13:19:28 -0400

    a BUNCH of organization and pause menu progress

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -5,7 +5,7 @@ func _on_play_pressed() -> void:
 
 
 func _on_rules_pressed() -> void:
-	get_tree().change_scene_to_file("res://how_to_play_page.tscn")
+	get_tree().change_scene_to_file("res://Scenes/how_to_play_page.tscn")
 
 
 func _on_quit_pressed() -> void:
