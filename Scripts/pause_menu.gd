b80cf99a31643ed0cfe88d60556c37ecc71240a3
abratu2226 <allstar555700@gmail.com>
2025-06-10 15:17:40 -0400

    pause menu progress

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,1 +1,16 @@
 extends Control
+
+@onready var main = $"../"
+
+
+
+func _on_resume_pressed():
+	main.pauseMenu()
+
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
